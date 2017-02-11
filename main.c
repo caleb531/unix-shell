467c045ad05092038eda5f9e4629afc676e7fedd
Caleb Evans <caleb@calebevans.me>
2017-02-10 17:03:37 -0800

    Write basic shell that echoes typed command

--- main.c
+++ main.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// The maximum length allowed for any line in the shell
+static const int MAX_LINE_LENGTH = 80;
+
+// Execute the given command (or display error if not a real command)
+void execCmd(char* cmd) {
+	if (strcmp(cmd, "") == 0) {
+		// Do nothing if the entered command is empty
+		return;
+	} else if (strcmp(cmd, "exit") == 0) {
+		// Exit the shell if `exit` command was given
+		exit(0);
+	}
+	// Merely print the command if non-empty
+	printf("%s\n", cmd);
+}
+
+// Wait for the user to enter a one-line command, executing what was entered if
+// possible (or erroring out if not possible)
+void getEnteredCmd() {
+	char *cmd = (char*)malloc(MAX_LINE_LENGTH * sizeof(char));
+	size_t bufferSize;
+	size_t cmdLength = getline(&cmd, &bufferSize, stdin);
+	if (cmdLength != -1) {
+		// Remove trailing newline from entered command (for conveniece)
+		cmd[cmdLength - 1] = '\0';
+		cmdLength--;
+		// Execute whatever command was entered (if possible)
+		execCmd(cmd);
+	} else {
+		// Exit if an error occurred while reading the line
+		printf("could not read line; exiting...");
+		exit(0);
+	}
+}
 
 int main() {
+
+	// The list where parsed CLI arguments will be stored
+	// TODO: move this somewhere else and do something useful with it
+	// char *cmdArgs[(MAX_LINE_LENGTH / 2) + 1];
+
+	// Run forever until shell exit
+	while (1) {
+		printf("osh> ");
+		fflush(stdout);
+		getEnteredCmd();
+	}
 
 	return 0;
 
